d1eb5a6273dce4967024f8694387a5c8097b54db
Felipe <feligaquino@gmail.com>
2017-09-05 12:28:43 -0300

    Primeira merda

---
+++ Odbc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace Odbc
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string ConexaoString = "driver={SQL Server};server=NETI-PC;uid=sa;pwd=12345678";
+
+            using (OdbcConnection Conexao = new OdbcConnection(ConexaoString))
+            {
+                Conexao.Open();
+                string Sql = @"IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = 'livraria') DROP DATABASE livraria";
+                OdbcCommand Cmd = new OdbcCommand(Sql, Conexao);
+                Cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
